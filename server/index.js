50a4376754deec9ee89ba8061502ca8b83bd9a9e
miseryturner <mmizari2019@mail.ru>
2022-08-10 15:43:31 +0500

    generic error handling

--- server/index.js
+++ server/index.js
@@ -5,11 +5,15 @@ const models = require('./models/models')
 const PORT = process.env.PORT || 5000
 const cors = require('cors')
 const router = require('./routes/index')
+const errorHendler = require('./middleware/ErrorHandlingMiddleware')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
+
+// Обработка ошибок, последний Middleware
+app.use(errorHendler)
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'WORKING!!' })
