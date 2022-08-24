5d6d90860c70cd2047f9e13afb943fb3f5f4c5f0
miseryturner <mmizari2019@mail.ru>
2022-08-24 09:33:57 +0500

    add user login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,9 +3,17 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const {User} = require('../models/models')
 
+const generateToken = (id, email, role, name) => {
+    return jwt.sign(
+        {id: id, email, role, name},
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
+
 class UserController {
     async registration(req, res, next) {
-        let {email, password, role, name} = req.body
+        let {email, password, name} = req.body
         if(!email || !password) {
             return next(ApiError.badRequest('Не корректный email или password'))
         }
@@ -16,25 +24,29 @@ class UserController {
         }
 
         const hashPassword = await bcrypt.hash(password, 5)
-        const user = await User.create({email, password: hashPassword, role, name})
-        const token = jwt.sign(
-            {id: user.id, email, role, name},
-            process.env.SECRET_KEY,
-            {expiresIn: '24h'}
-        )
+        const user = await User.create({email, password: hashPassword, name})
+        const token = generateToken(user.id, user.email, user.role, user.name)
         return res.json({token})
     }
 
-    async login(req, res) {
+    async login(req, res, next) {
+        const {email, password} = req.body
+        const user = await User.findOne({where: {email}})
+        if(!user) {
+            return next(ApiError.internal('Пользователь с таким именем не найден'))
+        }
+        
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if(!comparePassword) {
+            return next(ApiError.internal('Не верный пароль'))
+        }
 
+        const token = generateToken(user.id, user.email, user.role, user.name);
+        return res.json({token})
     }
 
     async check(req, res, next) {
-        const {id} = req.query
-        if (!id) {
-            return next(ApiError.badRequest('Не задан ID'))
-        }
-        res.json(id)
+        
     }
 }
 
